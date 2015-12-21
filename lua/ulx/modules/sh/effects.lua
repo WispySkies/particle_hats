d01563150f52ef29aa1649d73bbb6dda16c06a97
Timmy <github@timmy.ws>
2015-12-21 04:11:48 +0100

    Fix things

--- lua/ulx/modules/sh/effects.lua
+++ lua/ulx/modules/sh/effects.lua
@@ -1,6 +1,5 @@
 if SERVER then
     util.AddNetworkString( "ulx_particles" )
-    util.AddNetworkString( "ulx_particles_remove" )
 
     hook.Add( "PlayerInitalSpawn", "ulx_particles", function( ply )
         for _, ply in pairs(player.GetAll()) do
@@ -12,9 +11,6 @@ if SERVER then
             end
         end
     end )
-
-    local particles = { "superrare_beams1", "superrare_burning1", "superrare_burning2", "superrare_circling_heart", "superrare_circling_peacesign", "superrare_circling_skull", "superrare_circling_tf", "superrare_confetti_green", "superrare_confetti_purple", "superrare_ghosts", "superrare_flies", "superrare_plasma1", "superrare_plasma2", "superrare_greenenergy", "superrare_purpleenergy", "unusual_orbit_planets", "unusual_storm", "unusual_blizzard", "unusual_smoking", "unusual_bubbles", "unusual_orbit_shells", "unusual_orbit_nutsnbolts", "unusual_orbit_fire", "unusual_orbit_fire_dark", "unusual_fullmoon_cloudy", "unusual_orbit_jack_flaming", "unusual_bubbles_green", "unusual_storm_knives", "unusual_skull_misty", "unusual_fullmoon_cloudy_green", "unusual_storm_spooky", "unusual_storm_blood" }
-    concommand.Add( "particlehats_print_particles", function() PrintTable( particles ) end )
 end
 
 if CLIENT then
@@ -32,14 +28,15 @@ if CLIENT then
         target:StopParticleEmission()
         timer.Simple( 0, function() ParticleEffectAttach( particle, PATTACH_POINT_FOLLOW, target, attachment ) end )
     end )
-
-    net.Receive( "ulx_particles_remove", function()
-        local target = net.ReadEntity()
-        target:StopParticleEmission()
-    end)
 end
 
-local particles = { "superrare_beams1", "superrare_burning1", "superrare_burning2", "superrare_circling_heart", "superrare_circling_peacesign", "superrare_circling_skull", "superrare_circling_tf", "superrare_confetti_green", "superrare_confetti_purple", "superrare_ghosts", "superrare_flies", "superrare_plasma1", "superrare_plasma2", "superrare_greenenergy", "superrare_purpleenergy", "unusual_orbit_planets", "unusual_storm", "unusual_blizzard", "unusual_smoking", "unusual_bubbles", "unusual_orbit_shells", "unusual_orbit_nutsnbolts", "unusual_orbit_fire", "unusual_orbit_fire_dark", "unusual_fullmoon_cloudy", "unusual_orbit_jack_flaming", "unusual_bubbles_green", "unusual_bubbles_green", "unusual_storm_knives", "unusual_skull_misty", "unusual_fullmoon_cloudy_green", "unusual_storm_secret", "unusual_storm_spooky", "unusual_storm_blood", "generic_smoke" }
+local particles = { "superrare_beams1", "superrare_burning1", "superrare_burning2", "superrare_confetti_green", "superrare_confetti_purple", "superrare_ghosts", "superrare_flies", "superrare_plasma1", "superrare_plasma2", "superrare_greenenergy", "superrare_purpleenergy", "unusual_storm", "unusual_blizzard", "unusual_smoking", "unusual_bubbles", "unusual_orbit_nutsnbolts", "unusual_orbit_fire", "unusual_orbit_fire_dark", "unusual_bubbles_green", "unusual_storm_knives", "unusual_storm_spooky", "unusual_storm_blood" }
+
+if SERVER then
+    concommand.Add( "particlehats_print_particles", function()
+        PrintTable( particles )
+    end )
+end
 
 for i=1, #particles do
     PrecacheParticleSystem( particles[i] )
@@ -47,15 +44,8 @@ end
 
 function ulx.particle( player, target, particle, should_remove )
     if should_remove then
-        net.Start( "ulx_particles_remove" )
-        net.WriteEntity( target )
-        net.Broadcast()
-        return
-    end
-
-    if not particle then
-        ULib.tsayError( player, "No particle given! Particles printed to your console!" )
-        ULib.console( player, table.concat( particles, ", " ) )
+        target:StopParticles()
+        ulx.fancyLogAdmin( player, "#A removed particles for #T.", target )
         return
     end
 
@@ -70,7 +60,8 @@ end
 
 local particle = ulx.command( "Particle Effects", "ulx particle", ulx.particle, "!particle" )
 particle:addParam{ type=ULib.cmds.PlayerArg }
-particle:addParam{ type=ULib.cmds.StringArg, completes=particles, error="Invalid particle! \"%s\" specified!", ULib.cmds.optional }
+particle:addParam{ type=ULib.cmds.StringArg, completes=particles, error="Invalid particle! \"%s\" specified!", ULib.cmds.optional, ULib.cmds.restrictToCompletes }
+particle:addParam{ type=ULib.cmds.BoolArg, invisible=true }
 particle:defaultAccess( ULib.ACCESS_ADMIN )
 particle:help( "Gives yourself/or a target, a tf2 particle hat effect." )
 particle:setOpposite( "ulx stopparticle", {_, _, _, true}, "!stopparticle" )
